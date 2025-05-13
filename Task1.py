5bd039b78c122c79f4ac5ab4ff58ef5eb5a3038a
vinayakbenade <vinayakbenade@gmail.com>
2025-05-13 20:24:48 +0530

    Add files via upload
    tutedude_python_tasks

---
+++ Task1.py
@@ -0,0 +1,11 @@
+x = int(input("enter the first number: "))
+y = int(input("enter the second number: "))
+addition =  x + y
+substraction = x - y
+multiflication = x * y
+division = x/y
+print("\n")
+print("addition: ", addition)
+print("substraction: ", substraction)
+print("multifilication: ", multiflication)
+print("division: ", division)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+x=input(" enter your  first name: ")
+y=input(" enter your  last name: ")
+add=x+' '+y
+print('hello,', add +'!', 'Welcome to the python program')
